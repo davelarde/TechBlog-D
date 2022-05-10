d17adccddc1315ebe3e46fb5f7c60d88e3f9fb93
danivelarde <danielavelarde4@gmail.com>
2022-05-09 20:38:42 -0400

    server.js

--- server.js
+++ server.js
@@ -7,3 +7,39 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Tech blog secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// INSTEAD OF LINE 5:
+// app.use(require('./controllers/'));
+
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
