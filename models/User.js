0e7f883dd9cbb1ce006f7b895f1a0c958b1065fe
danivelarde <danielavelarde4@gmail.com>
2022-05-10 21:22:44 -0400

    models

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-onst { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/config');
 
@@ -25,3 +25,26 @@ class User extends Model {
       allowNull: false,
       validate: {
         len: [8],
+    },
+},
+},
+{
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
